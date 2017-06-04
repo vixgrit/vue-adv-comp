36a69c45e332e4952a525afc2e7489a641a66cd7
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-04 17:57:03 -0400

    pass content with slots

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-               
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <appQuote>
+          <h2>{{ quoteTitle }}</h2>
+          <p>A wonderful Quote</p>
+        </appQuote>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-
-    export default {
-        
+  import Quote from './components/Quote.vue';
+  export default {
+    data() {
+      return {
+        quoteTitle: 'The Quote'
+      }
+    },
+    components: {
+      'appQuote': Quote
     }
+  }
 </script>
 
-<style>
+<style scoped>
 </style>
